e06843f93dccfdcd6b0b182fbb9102bbf458f0a7
Matias <eiglesias@grupoassa.com>
2015-02-11 10:21:27 -0300

    button-functions

---
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+   $('.button').fadeTo('slow', 0);
+    $('.custom-logo').fadeTo('slow', 0);
+    $('.custom-button').fadeTo('slow', 0);
+});
--- RetailApp/RetailApp/Content/Media/Script1.js
+++
@@ -1,4 +0,0 @@
-
-$(document).ready(function () {
-    $('.button').fadeOut('400');
-});
