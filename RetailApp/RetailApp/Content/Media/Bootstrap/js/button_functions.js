55684e1f6723e45b1bf07027428d5e63acde1f70
Matias <eiglesias@grupoassa.com>
2015-02-13 12:55:49 -0300

    almost finished login page

--- RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.button').click(function () {
         $(this).fadeTo('very fast', 0.85);
         $(this).fadeTo('very fast', 1);
     });
     $('span.pull-right a').click(function () {    
         if ($('#loginModal div.modal-dialog div.modal-footer span.pull-right a').text() === 'No deseas loguear con facebook?') {
-            $('#loginModal div.modal-dialog div.modal-body').html("<div class='input-group input-group-lg'><span class='input-group-addon' id='sizing-addon1'>@</span><input type='text' class='form-control' placeholder='E-mail' aria-describedby='sizing-addon1'></div><button type='button' class='pull-right'>Enviar</button>");
+            $('#loginModal div.modal-dialog div.modal-body').html("<div class='input-group input-group-lg'><span class='input-group-addon' id='sizing-addon1'>@</span><input type='text' class='form-control' placeholder='E-mail' aria-describedby='sizing-addon1'><button type='button' class='pull-right'>Enviar</button></div>");
             $('#loginModal div.modal-dialog div.modal-footer span.pull-right a').html("Volver a loguear con facebook?");
         } else {
             $('#loginModal div.modal-dialog div.modal-body').html("<button class='button' onclick='checkFacebookLogin();'><div class='custom-logo'>f</div><div class='custom-button'>Sign in with Facebook</div></button>");
