8ed4f73ae2c2ca6a280806fa566f2f003df8ea13
Matias <eiglesias@grupoassa.com>
2015-02-20 09:41:29 -0300

    Questionnaire and prize pages

--- RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
-    $('.button').click(function () {
-        $(this).slideUp(500);
+    $('button.button').mouseenter(function () {
+        $(this).css("background-color", "#1e62d0");
+        $(this).css("transition", "0.5s");
+        $(this).css("border", "1px solid black");
+        $('.custom-logo').css("background-color", "#1e62d0");
+        $('.custom-logo').css("transition", "0.5s");
+        $('.custom-logo').css("border-right", "2px solid #022248");
+        $('.custom-button').css("background-color", "#1e62d0");
+        $('.custom-button').css("transition", "0.5s");
+        $('.custom-button').css("border-left", "2px solid #022248");
     });
+    $('button.button').mouseleave(function () {
+        $(this).css("background-color", "#022248");
+        $(this).css("transition", "0.5s");
+        $('.custom-logo').css("background-color", "#022248");
+        $('.custom-logo').css("transition", "0.5s");
+        $('.custom-logo').css("border-right", "2px solid #030f1e");
+        $('.custom-button').css("background-color", "#022248");
+        $('.custom-button').css("transition", "0.5s");
+        $('.custom-button').css("border-left", "2px solid #030f1e");
+    });
+
+    $('button.button').click(function () {
+        
+    });
+
     $('span.pull-right a').click(function () {
         $('#modal-toSlide').slideUp(500, function () {
             if ($('#loginModal div.modal-dialog div.modal-footer span.pull-right a').text() === 'No deseas loguear con facebook?') {                
@@ -13,7 +36,6 @@ $(document).ready(function () {
                 }
         });        
         $('#modal-toSlide').slideDown(500);
-        setTimeout(function () { $('.button').slideDown(500); }, 500);
     });  
 });
 
--- RetailApp/RetailApp/Content/Media/Bootstrap/js/questionnaire.js
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/questionnaire.js
@@ -1,3 +1,15 @@
 ﻿$(function () {
     $('a[title]').tooltip();
 });
+
+$(document).ready(function () {
+    $('.round-tabs,.liner').hide(0,function () {
+        $('.round-tabs').slideDown(1000, function () {
+            $('.liner').fadeTo(2000,1);
+        });
+    });
+    if ($('li.active a span.round-tabs').text() === '5') {      
+       /*("body").append("<a href='#'>Submit</a>");*/
+    }
+
+});
