ea505a64a8d4b39cf140eec4fd8d43303fd6e8c9
unknown <eiglesias@ASSA55.grupoassa>
2015-02-26 11:02:22 -0300

    Login page

--- RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/button_functions.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $('#loginModal div.modal-dialog div#modal-email').hide(0);
+
     $('button.button').mouseenter(function () {
         $(this).css("background-color", "#1e62d0");
         $(this).css("transition", "0.5s");
@@ -21,19 +23,18 @@ $(document).ready(function () {
         $('.custom-button').css("border-left", "2px solid #030f1e");
     });
 
-    $('button.button').click(function () {
-        
+    $('button.button').click(function () {   
     });
 
     $('span.pull-right a').click(function () {
         $('#modal-toSlide').slideUp(500, function () {
-            if ($('#loginModal div.modal-dialog div.modal-footer span.pull-right a').text() === 'No deseas loguear con facebook?') {                
-                $('#loginModal div.modal-dialog div.modal-body').html("<form><div class='form-group'><div id='email-title'><label for='inputEmail3'>Email address</label></div><div class='text-center' id='email-box'><input type='email' class='form-control' id='inputEmail3' placeholder='Enter your email'></div></div><div class='text-center'><a href='#' class='email-button'>Submit</a></div></form>");
-                    $('#loginModal div.modal-dialog div.modal-footer span.pull-right a').html("<a href='#' class='link-style'>Volver a loguear con facebook?</a>");
-                } else {           
-                    $('#loginModal div.modal-dialog div.modal-body').html("<button class='button' onclick='checkFacebookLogin();'><div class='custom-logo'>f</div><div class='custom-button'>Sign in with Facebook</div></button>");
-                    $('#loginModal div.modal-dialog div.modal-footer span.pull-right a').html("No deseas loguear con facebook?");
-                }
+            if ($('#loginModal div.modal-dialog div.modal-footer span.pull-right a').text() === 'No deseas loguear con facebook?') {
+                $('#modal-facebook').hide(0);
+                $('#modal-email').show(0);                                            
+            } else {
+                $('#modal-email').hide(0);
+                $('#modal-facebook').show(0);
+            }
         });        
         $('#modal-toSlide').slideDown(500);
     });  
--- RetailApp/RetailApp/Controllers/HomeController.cs
+++ RetailApp/RetailApp/Controllers/HomeController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq;
+using RetailApp.Database;
+using System.Web.UI.WebControls;
+using System.Collections.Specialized;
+using System.Net.Mail;
+using System.Web.UI;
+
 
 namespace RetailApp.App_Start
 {
@@ -16,5 +23,28 @@ namespace RetailApp.App_Start
             return View();
         }
 
-    }
+        public ActionResult loginWithEmail(FormCollection form)
+        {
+            using (var usr = new RetailAppEntities2())
+            {
+
+                var email = form["email"].ToString();
+                /*var text = usr.USER.Where(m => m.Email == form["inputEmail3"]);*/
+                
+                /*if(){
+                    
+                }
+                             
+                    var msg = new MailMessage();
+                    msg.To.Add(user.Email);
+                    msg.IsBodyHtml = false;
+                    msg.Subject = "Test email";
+                    msg.Body = "Hola " + user.Apellido + ", " + user.Nombre + " te ganaste un premio.. ";
+                    SmtpClient smtpClient = new SmtpClient();
+                    smtpClient.Send(msg);            
+            }*/
+                return RedirectToAction("Index");
+            }
+        }
+        }
 }
