9bb1d6fa790d84202d06facd9e59b216613a7aad
unknown <eiglesias@ASSA55.grupoassa>
2015-03-02 16:41:41 -0300

    merge

---
+++ RetailApp/RetailApp/Content/Media/Bootstrap/js/Prize.js
@@ -0,0 +1,20 @@
+﻿
+
+$(document).ready(function () {
+
+    var showText = function (target, message, index, interval) {
+        if (index < message.length) {
+            $(target).append(message[index++]);
+            setTimeout(function () {
+                showText(target, message, index, interval);
+            }, interval);
+        }
+    }
+
+    $('#modal-toSlide').slideUp(0, function () {
+        $('#modal-toSlide').slideDown(3000,
+            showText("#prize-header","Te ganaste un premio!", 0, 100));
+    });
+
+    
+});
