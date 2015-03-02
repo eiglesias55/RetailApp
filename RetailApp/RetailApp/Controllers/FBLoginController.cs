5662e7a8eb704337c37a99e9c0bf1f0360e699c6
seblerom <soloban88@gmail.com>
2015-03-02 16:35:15 -0300

    FBLogin controller change status when login

---
+++ RetailApp/RetailApp/Controllers/FBLoginController.cs
@@ -0,0 +1,33 @@
+﻿using RetailApp.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RetailApp.Controllers
+{
+    public class FBLoginController : ApiController
+    {
+        [HttpGet]
+        public String Login(String Email, String NewEmail)
+        {
+            try
+            {
+                using (var csx = new RetailAppEntities()) {
+                    USER u = csx.USER.SingleOrDefault(user => user.Email == Email);
+                    csx.Entry(u).State = System.Data.Entity.EntityState.Modified;
+                    if (Email != NewEmail) { u.EmailSecundario = NewEmail; }
+                    u.Status = 3;
+                    u.Fecha = DateTime.Now;
+                    csx.SaveChanges();
+                }
+                return "Ok";
+            }
+            catch (Exception) {
+                return "Error";
+            }
+        }
+    }
+}
