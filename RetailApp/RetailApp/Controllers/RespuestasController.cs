41cbc25fd6fc18d0410243600d97ecd55636b64c
seblerom <soloban88@gmail.com>
2015-03-03 11:17:48 -0300

    Respuestas controller

--- RetailApp/RetailApp/Controllers/RespuestasController.cs
+++ RetailApp/RetailApp/Controllers/RespuestasController.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using RetailApp.Database;
 namespace RetailApp.Controllers
 {
     public class RespuestasController : ApiController
     {
+        [HttpGet]
+        public string GuardarRespuestas(string Email, int IdPregunta, int NumeroOpcion)
+        {
+            try
+            {
+                using (var csx = new RetailAppEntities())
+                {
+                    RESPUESTA r = csx.RESPUESTA.SingleOrDefault(rta => rta.Email == Email && rta.Id == IdPregunta);
+                    if (r != null){
+                        csx.Entry(r).State = System.Data.Entity.EntityState.Modified;
+                        r.Numero = NumeroOpcion; 
+                    }else{
+                        RESPUESTA resp = new RESPUESTA();
+                        resp.Email = Email;
+                        resp.Numero = NumeroOpcion;
+                        resp.Id = IdPregunta;
+                        csx.RESPUESTA.Add(resp);
+                    }
+                    csx.SaveChanges();
+                }
+                return "Ok";
+            }
+            catch(Exception) {
+                return ("Error");
+            }
+        }
     }
 }
