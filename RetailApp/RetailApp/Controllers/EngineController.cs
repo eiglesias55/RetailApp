8d5d346cf1cb1e6473f4d4d64e5fe30e5ceeb33f
Matias <eiglesias@grupoassa.com>
2015-03-10 10:57:45 -0300

    Analytics running ok

--- RetailApp/RetailApp/Controllers/EngineController.cs
+++ RetailApp/RetailApp/Controllers/EngineController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Facebook;
-using System.Collections.Generic;
 
 namespace RetailApp.Controllers
 {
@@ -14,27 +13,39 @@ namespace RetailApp.Controllers
         [HttpGet]
         public String SendData(String Token) {
             var client = new FacebookClient(Token);
-            dynamic JsonLikes = client.Get("me/likes");
-            dynamic JsonCategories = client.Get("search?type=placetopic&topic_filter=all&fields=id,name,parent_ids");
-            var categories = JsonCategories.data;
+            dynamic JsonLikes = client.Get("me/likes?fields=name,id,category_list&limit=1000");
             var likes = JsonLikes.data;
             Dictionary<string,int> categoriesDictionary = new Dictionary<string,int>();
-            foreach(var category in categories){
-                try
-                {
-                    categoriesDictionary.Add((String)category.name, 0);
-                }
-                catch (ArgumentException)
-                {
-                    //No lo agrega
-                }
-            }
+
             foreach (var like in likes)
                 {
-                    int cantidad;
-                    if (categoriesDictionary.TryGetValue((String)like.category,out cantidad)) { 
-                       categoriesDictionary[(String)like.category] = cantidad++;
-                    };
+                    try
+                    {
+                        dynamic parentId = client.Get(like.category_list[0].id + "?fields=name,parent_ids");
+                        string parentCategory = "";
+                        try
+                        {
+                            while (parentId.parent_ids[0] != null) 
+                            {
+                                parentId = client.Get(parentId.parent_ids[0] + "?fields=name,parent_ids");
+                                parentCategory = parentId.name;
+                            }
+                        }
+                        catch (Exception) 
+                        {
+                            int cantidad;
+                            if (categoriesDictionary.TryGetValue(parentCategory, out cantidad))
+                            {
+                                categoriesDictionary[parentCategory] = ++cantidad;
+                            }
+                            else 
+                            {
+                                categoriesDictionary.Add(parentCategory, 1);
+                            }
+                        }
+                    }
+                    catch (Exception) { 
+                    }
                 }                 
             return "Ok";
         }
